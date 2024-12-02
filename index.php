cd686fb4e01233063ff0992b45d190d50321d53e
jsnodees <202280134@psu.palawan.edu.ph>
2024-12-02 21:47:58 +0800

    styled the index file

--- index.php
+++ index.php
@@ -21,6 +21,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
     <title>MATH QUIZ</title>
 </head>
 <body>
@@ -67,7 +68,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     Multiplication
                 </label>
             </fieldset>
-            
+
             <label for="numItem">Number of Items: </label>
             <input type="number" name="numitem" id="numitem" placeholder="e.g., 10" required>
 
