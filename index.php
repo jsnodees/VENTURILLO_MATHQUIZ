38270cafa353f40e5fc7a5e28691d3ca5590db1a
jsnodees <202280134@psu.palawan.edu.ph>
2024-12-02 21:12:37 +0800

    added html and a header

--- index.php
+++ index.php
@@ -16,3 +16,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MATH QUIZ</title>
+</head>
+<body>
+    <header>
+        <h1>MATH QUIZ</h1>
+    </header>
+    
+</body>
+</html>
+
