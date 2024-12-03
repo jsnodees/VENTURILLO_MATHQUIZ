cb73b1922a6ced51aae173d4fa6a2b3b501e5b74
jsnodees <202280134@psu.palawan.edu.ph>
2024-12-03 09:11:15 +0800

    code to reset the session for a new quiz

---
+++ end.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+
+$numItems = $_SESSION['numItems'];
+$correct = $_SESSION['correct'];
+$grade = ($correct / $numItems) * 50 + 50;
+
+session_destroy();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Quiz Results</title>
+<body>
+<header>
+    <h1>Math Quiz Results</h1>
+</header>
+
+<div class="form-container">
+    <p><strong>Correct Answers:</strong> <?php echo $correct; ?></p>
+    <p><strong>Wrong Answers:</strong> <?php echo $_SESSION['wrong']; ?></p>
+    <p><strong>Your Grade:</strong> <?php echo $grade; ?>%</p>
+
+    <form action="mathy.php">
+        <button type="submit">Take Quiz Again</button>
+    </form>
+</div>
+</body>
+</html>
+
